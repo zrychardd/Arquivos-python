f07515e038de9ccb3746919e4c304ad17face5c3
zrychardd <rychardeduardos@gmail.com>
2025-03-07 20:38:32 -0300

    Add files via upload

---
+++ python/python4.py
@@ -0,0 +1,10 @@
+import random as rd
+
+numero_secreto = rd.randint(1 , 100)
+
+tentativa = int(input("Escolha um numero de 1 até 100"))
+
+if tentativa == numero_secreto:
+    print("Você acertou o número secreto") 
+    
+    else:
